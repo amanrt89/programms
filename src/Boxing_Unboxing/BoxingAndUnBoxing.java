7c00f90bbee5e59f58102703876014e795d09d6e
amanrt89 <rttekade.aman99@gmail.com>
2024-12-18 20:35:22 +0530

    1st Commit

---
+++ src/Boxing_Unboxing/BoxingAndUnBoxing.java
@@ -0,0 +1,17 @@
+package Boxing_Unboxing;
+
+public class BoxingAndUnBoxing {
+
+	public static void main(String[] args) 
+	{
+      int a=10;
+      Integer x=a;
+      System.out.println(x);//Boxing
+     
+      Integer d=20 ;
+      int f=d;
+      System.out.println(f);//UnBoxing
+		
+	}
+
+}
